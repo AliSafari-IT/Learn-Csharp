8fd9b802a5644b53e80a1c96da91140438f37224
asafarim <asafarim.it@gmail.com>
2024-01-20 23:24:18 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Drawing;
+
+class Vehicle  // base class (parent) 
+{
+    public string brand = "Ford";  // Vehicle field
+    public static void Honk()             // Vehicle method 
+    {
+        Console.WriteLine("Tuut, tuut!");
+    }
+
+    public static ConsoleColor Color()
+    {
+
+        var val = new MainCar().mainColor;
+        Console.WriteLine(val);
+        if (val == ConsoleColor.Green)
+        {
+            return ConsoleColor.DarkGreen;
+        }
+
+        return val;
+    }
+}
+
+class Car : Vehicle  // derived class (child)
+{
+    public string modelName = "Mustang";  // Car field
+}
+
+
+class MainCar
+{
+    public ConsoleColor mainColor = ConsoleColor.Red;
+
+    static void Main()
+    {
+        Car myCar = new();
+        var vc = Vehicle.Color();
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write($"{myCar.brand} {myCar.modelName} (Color: ");
+        Console.ForegroundColor = vc;
+        Console.Write($" {vc}");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine($")");
+        Vehicle.Honk();
+    }
+}
